055756d03fff1cac600e35788a1abc335ce3efaf
mamazu <14860264+mamazu@users.noreply.github.com>
2025-04-08 21:22:02 +0200

    Fixing the compiler pass (#161)

--- src/CompilerPass/AddingTypesToAdjustmentClearerPass.php
+++ src/CompilerPass/AddingTypesToAdjustmentClearerPass.php
@@ -17,12 +17,29 @@ final class AddingTypesToAdjustmentClearerPass implements CompilerPassInterface
 
         // Getting the new list of adjustment types to clear
         $listOfAdjustmentsToClear = $clearerDefinition->getArgument(0);
-        if (1 === preg_match('/^%(.*)%$/', $listOfAdjustmentsToClear, $matches)) {
-            $listOfAdjustmentsToClear = $container->getParameter($matches[1]);
+
+        // If the argument is a string then it's the name of the parameter where the services are configured
+        // We then just need to modify the parameter.
+        if (is_string($listOfAdjustmentsToClear)) {
+            $this->addClearerToContainerParameter($container, $listOfAdjustmentsToClear);
+
+            return;
         }
+
+        // BC: For Symfony < 6 (modifing the service directly)
         $listOfAdjustmentsToClear[] = CustomerOptionRecalculator::CUSTOMER_OPTION_ADJUSTMENT;
 
         // Setting the new list as the new definition
         $clearerDefinition->setArgument(0, $listOfAdjustmentsToClear);
+    }
+
+    private function addClearerToContainerParameter(ContainerBuilder $container, string $parameterName): void
+    {
+        assert(1 === preg_match('/^%(.*)%$/', $parameterName, $matches));
+        $parameterName = $matches[1];
+        $listOfAdjustmentsToClear = $container->getParameter($parameterName);
+        $listOfAdjustmentsToClear[] = CustomerOptionRecalculator::CUSTOMER_OPTION_ADJUSTMENT;
+
+        $container->setParameter($parameterName, $listOfAdjustmentsToClear);
     }
 }
